5e8a14af76a3f6f883c895b0196ca91e128697a7
David <david@outlier.org>
2019-06-14 13:26:16 -0700

    feat: add user-agent endpoint

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000
 
 const server = http.createServer((req, res) => {
   if (req.url === '/') return respondHello(req, res)
-  if (req.url.match(/^\/b64\//)) return respondBase64(req, res)
+  if (req.url === '/user-agent') return respondUserAgent(req, res)
 
   res.end()
 })
@@ -13,12 +13,12 @@ function respondHello (req, res) {
   res.end(JSON.stringify({ msg: 'hello' }))
 }
 
+<<<<<<< HEAD
 function respondBase64 (req, res) {
   const phrase = req.url.replace(/^\/b64\//, '')
   res.end(JSON.stringify({ b64: Buffer.from(phrase).toString('base64') }))
 }
 
 server.listen(PORT)
-console.log(`Server listening on port ${PORT}`)
 
 if (require.main !== module) module.exports = server
